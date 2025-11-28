d7145973a3e1c0fea00f2d71e1fd695dc2f1029e
hathbanger <hathbanger.ah@gmail.com>
2025-11-28 12:37:19 -0700

    Update httpcat to version 0.0.11

--- Formula/httpcat.rb
+++ Formula/httpcat.rb
@@ -1,8 +1,8 @@
 class Httpcat < Formula
   desc "CLI tool for interacting with httpcat agent - create, buy, and sell tokens with x402 payments"
   homepage "https://github.com/hathbanger/httpcat-cli"
-  url "https://registry.npmjs.org/httpcat-cli/-/httpcat-cli-0.0.10.tgz"
-  sha256 "d230d5b0576c5744e7a96e650481bedb0ae507eb37b047489f1f9ee8baab4460"
+  url "https://registry.npmjs.org/httpcat-cli/-/httpcat-cli-0.0.11.tgz"
+  sha256 "38639ddcc875fc9816997fa8940e931dfd5aa76ba216438d57f645a4419848e4"
   license "MIT"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class Httpcat < Formula
   end
 
   test do
-    assert_match "0.0.10", shell_output("#{bin}/httpcat --version")
+    assert_match "0.0.11", shell_output("#{bin}/httpcat --version")
   end
 end
